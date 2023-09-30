5fcef702b216082c3c2c8a12ef0cfc140dd0314f
Pruthvi Mehta <qthedesperate@gmail.com>
2023-09-30 21:52:04 +0100

    added image output

--- classifier_script.py
+++ classifier_script.py
@@ -32,3 +32,31 @@ print(paper_files[:10])
 
 scissors_files = os.listdir(scissors_dir)
 print(scissors_files[:10])
+
+#Iterate over the training images
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+pic_index = 2
+
+
+next_rock = [os.path.join(rock_dir, fname) 
+                for fname in rock_files[pic_index-2:pic_index]]
+next_paper = [os.path.join(paper_dir, fname) 
+                for fname in paper_files[pic_index-2:pic_index]]
+next_scissors = [os.path.join(scissors_dir, fname) 
+                for fname in scissors_files[pic_index-2:pic_index]]
+
+print('ROCK NAME',next_rock)
+print('PAPER NAME',next_paper)
+print('SCISSORS NAME',next_scissors)
+print(next_rock+next_paper+next_scissors)
+for i, img_path in enumerate(next_rock+next_paper+next_scissors):
+  print(i,img_path)
+  img = mpimg.imread(img_path)
+  plt.imshow(img)
+  plt.axis('Off')
+  plt.show()
+
+
