c7a1ff51077c1ae8970e862ac580e2ec6b0ea99a
Pruthvi Mehta <qthedesperate@gmail.com>
2023-10-14 13:40:39 +0100

    image predictor script works on external test data

--- image_predictor.py
+++ image_predictor.py
@@ -1,18 +1,16 @@
 import numpy as np
-from google.colab import files
-from keras.preprocessing import image
+import tensorflow as tf
+import keras.utils as image
 
-uploaded = files.upload()
+reconstructed_model = tf.keras.models.load_model("/home/pruthvi/Desktop/tensorf\
+low_project/rps.h5")
 
-for fn in uploaded.keys():
- 
-  # predicting images
-  path = fn
-  img = image.load_img(path, target_size=(150, 150))
-  x = image.img_to_array(img)
-  x = np.expand_dims(x, axis=0)
+# predicting images
+path = "/home/pruthvi/Desktop/tensorflow_project/test_data/external_testing_data/italian.png"
+img = image.load_img(path, target_size=(150, 150))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
 
-  images = np.vstack([x])
-  classes = model.predict(images, batch_size=10)
-  print(fn)
-  print(classes)
+images = np.vstack([x])
+classes = reconstructed_model.predict(images, batch_size=10)
+print(classes)
