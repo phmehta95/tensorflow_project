8f24227e0a3f5d8433e2d750d564dca187822439
Pruthvi Mehta <qthedesperate@gmail.com>
2023-10-15 14:13:26 +0100

    fkdjskldfdj

--- image_predictor.py
+++ image_predictor.py
@@ -1,16 +1,22 @@
 import numpy as np
 import tensorflow as tf
 import keras.utils as image
+from matplotlib import pyplot as plt
+from matplotlib import image as mpimg
 
-reconstructed_model = tf.keras.models.load_model("/home/pruthvi/Desktop/tensorflow_project/rps.h5")
+reconstructed_model = tf.keras.models.load_model("/home/pruthvi/Desktop/tensorflow_project/rps_nodropout.h5")
 
 # predicting images
-path = "/home/pruthvi/Downloads/rockhand.jpg"
+path = "/home/pruthvi/Downloads/scissorhand.jpg"
 img = image.load_img(path, target_size=(150, 150))
+plt.imshow(img)
+plt.show()
+
+
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
-
 images = np.vstack([x])
+
 classes = reconstructed_model.predict(images, batch_size=10)
 print(classes)
 
