76a658243e0800d1610ebe2f09a9726b3e0a5cef
Pruthvi Mehta <qthedesperate@gmail.com>
2023-10-15 21:26:11 +0100

    importing pixellib to remove bg from hand images

--- image_predictor.py
+++ image_predictor.py
@@ -3,30 +3,45 @@ import tensorflow as tf
 import keras.utils as image
 from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
+import os
+import sys
+import time
+import pixellib
+
+from pixellib.tune_bg import alter_bg
 
 reconstructed_model = tf.keras.models.load_model("/home/pruthvi/Desktop/tensorflow_project/rps_nodropout.h5")
 
+directory = "/home/pruthvi/Desktop/tensorflow_project/test_data/external_testing_data/"
+
+for filename in os.listdir(directory): 
+    f = os.path.join(directory, filename)
+    print(f)
+
+#    change_bg = alter_bg()
+#    change_bg.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
+#    change_bg.color_bg(f, colors = (0,128,0), output_image_name = f_out)
+
 # predicting images
-path = "/home/pruthvi/Downloads/scissorhand.jpg"
-img = image.load_img(path, target_size=(150, 150))
-plt.imshow(img)
-plt.show()
+    img = image.load_img(f, target_size=(150, 150))
+    plt.imshow(img)
+    plt.show()
+    time.sleep(3)
 
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    images = np.vstack([x])
 
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-images = np.vstack([x])
+    classes = reconstructed_model.predict(images, batch_size=10)
+    print(classes)
 
-classes = reconstructed_model.predict(images, batch_size=10)
-print(classes)
+    if classes.flat[0] == 1.0:
+        print("PAPER")
 
-if classes.flat[0] == 1.0:
-    print("PAPER")
+    if classes.flat[1] == 1.0:
+        print("ROCK")
 
-if classes.flat[1] == 1.0:
-    print("ROCK")
-
-if classes.flat[2] == 1.0:
-    print("SCISSORS")
+    if classes.flat[2] == 1.0:
+        print("SCISSORS")
 
         
