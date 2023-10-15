1b66239cbcb8ea164117caacd8ce2ec61eeaf8ea
Pruthvi Mehta <qthedesperate@gmail.com>
2023-10-15 13:35:53 +0100

    added print statement depending on result

--- image_predictor.py
+++ image_predictor.py
@@ -2,11 +2,10 @@ import numpy as np
 import tensorflow as tf
 import keras.utils as image
 
-reconstructed_model = tf.keras.models.load_model("/home/pruthvi/Desktop/tensorf\
-low_project/rps.h5")
+reconstructed_model = tf.keras.models.load_model("/home/pruthvi/Desktop/tensorflow_project/rps.h5")
 
 # predicting images
-path = "/home/pruthvi/Desktop/tensorflow_project/test_data/external_testing_data/italian.png"
+path = "/home/pruthvi/Downloads/rockhand.jpg"
 img = image.load_img(path, target_size=(150, 150))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
@@ -14,3 +13,14 @@ x = np.expand_dims(x, axis=0)
 images = np.vstack([x])
 classes = reconstructed_model.predict(images, batch_size=10)
 print(classes)
+
+if classes.flat[0] == 1.0:
+    print("PAPER")
+
+if classes.flat[1] == 1.0:
+    print("ROCK")
+
+if classes.flat[2] == 1.0:
+    print("SCISSORS")
+
+        
